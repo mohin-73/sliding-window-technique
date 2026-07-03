8ea36cc61c1f16944f3860ff32d7d71188345240
Eklas Mohin <mohin73k@gmail.com>
2026-07-03 12:41:46 +0600

    LeetCode 713. Subarray Product Less Than K

---
+++ subarray_product_less_than_k.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
+        int n = static_cast<int>(nums.size());
+        int left = 0, right = 0;
+        int product = 1, cnt = 0;
+        while (right < n) {
+            product *= nums[right];
+            while (product >= k) {
+                product /= nums[left++];
+            }
+            cnt += right++ - left + 1;
+        }
+        return cnt;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<int> nums = {10, 5, 2, 6};
+    int k = 100;
+    Solution solve;
+    cout << solve.numSubarrayProductLessThanK(nums, k) << '\n';
+    return 0;
+}
+
+/*
+    ------------------
+    Problem Statement:
+    ------------------
+
+    Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+
+    ----------
+    Example 1:
+    ----------
+
+    Input: nums = [10,5,2,6], k = 100
+    Output: 8
+    Explanation: The 8 subarrays that have product less than 100 are:
+    [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+    Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+
+    ----------
+    Example 2:
+    ----------
+
+    Input: nums = [1,2,3], k = 0
+    Output: 0
+    
+    ------------
+    Constraints:
+    ------------
+
+    1 <= nums.length <= 3 * 10^4
+    1 <= nums[i] <= 1000
+    0 <= k <= 10^6
+*/
